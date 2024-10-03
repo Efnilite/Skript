8c2a806cd3d4cb80a0f812b932e76d2cbf928787
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-03 23:16:09 +0200

    gag

--- src/main/java/ch/njol/skript/lang/Simplifiable.java
+++ src/main/java/ch/njol/skript/lang/Simplifiable.java
@@ -2,6 +2,11 @@ package ch.njol.skript.lang;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents an expression that can be simplified to a {@link Literal} at parse-time.
+ *
+ * @param <T> The type of the literal.
+ */
 @FunctionalInterface
 public interface Simplifiable<T> {
 
